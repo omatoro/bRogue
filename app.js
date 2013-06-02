b936b29eaa95640c8e1d5521af30159d9d5f27ff
omatoro <okayama08039076173@okayama-no-MacBook-Air.local>
2013-06-02 19:04:49 +0900

    修正

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8001);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
