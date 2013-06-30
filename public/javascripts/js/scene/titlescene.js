3a48d0b4ea2a5c299410570eb92e77168c5e305c
omatoro <okayama@grincode.com>
2013-07-01 03:33:31 +0900

    tmlib.jsが対応してくれたので修正

--- public/javascripts/js/scene/titlescene.js
+++ public/javascripts/js/scene/titlescene.js
@@ -19,50 +19,6 @@
             this.addEventListener("pointingend", function(e) {
                 this._onClickOpenButton();
             }.bind(this));
-
-            // 直接onmenuselectedイベントからreplaceSceneを呼ぶと、popSceneと交錯するのでエラー
-            this.isNextScene = false;
-        },
-
-        update: function (app) {
-            if (this.isNextScene) {
-                // セーブされている階層があるか調べる
-                var saveData = this._loadSaveData();
-                if (saveData) {
-                    var stairs = 1;//saveData.saveData.stairs;
-                }
-                else {
-                    var stairs = 1; // 1階から開始
-                }
-                ns.STAIRS = stairs;
-
-                // デバッグ用
-                // var equipButton = tm.app.GlossyButton(200, 60, "gray", "データ削除").addChildTo(this);
-                // equipButton.position.set(ns.SCREEN_WIDTH-200, ns.SCREEN_HEIGHT-200);
-                // equipButton.addEventListener("pointingend", function(e) {
-                //     localStorage.removeItem("b-rogue");
-                // });
-                
-                // シーンの切り替え
-                var loadingScene = ns.AsyncLoading({
-                    width:        this.width,
-                    height:       this.height,
-                    assets:       MAIN_ASSET,
-                    loadingScene: ns.EffectLoadingScene,
-                    nextScene:    ns.MainScene,
-                },function (postLoadingFunc) {
-                    var self = this;
-                    var socket = self.socket;
-                    socket.emit("getMapData", stairs);
-                    // mapデータ取得
-                    socket.on("gotMapData", function (data) {
-                        console.log("gotMapData");
-                        this.mapData = data;
-                        postLoadingFunc();
-                    }.bind(self));
-                }.bind(ns.gameEvent)); // 仕方なくここで通信系の処理を書く
-                app.replaceScene(loadingScene);
-            }
         },
 
         // MainSceneと処理もろかぶり...　あとで考える
@@ -105,17 +61,60 @@
 
             dialog.onmenuselected = function(e) {
                 switch (menu[e.selectIndex]) {
-                    case "ゲームスタート(Twitter認証済み)":
-                    case "ゲームスタート(名無し)":
-                        this.isNextScene = true;
-                        break;
                     case "Twitter認証":
-                        window.open('/auth/twitter');
+                        window.open('/auth/twitter', "_blank");
                         break;
                     default :
                         break;
                 }
-                console.log(menu[e.selectIndex]);
+                this.lastSelection = e.selectIndex;
+            }.bind(this);
+
+            dialog.onmenuclosed = function(e) {
+                switch (menu[e.selectIndex]) {
+                    case "ゲームスタート(Twitter認証済み)":
+                    case "ゲームスタート(名無し)":
+                        // セーブされている階層があるか調べる
+                        var saveData = this._loadSaveData();
+                        if (saveData) {
+                            var stairs = 1;//saveData.saveData.stairs;
+                        }
+                        else {
+                            var stairs = 1; // 1階から開始
+                        }
+                        ns.STAIRS = stairs;
+
+                        // デバッグ用
+                        // var equipButton = tm.app.GlossyButton(200, 60, "gray", "データ削除").addChildTo(this);
+                        // equipButton.position.set(ns.SCREEN_WIDTH-200, ns.SCREEN_HEIGHT-200);
+                        // equipButton.addEventListener("pointingend", function(e) {
+                        //     localStorage.removeItem("b-rogue");
+                        // });
+
+                        // シーンの切り替え
+                        var loadingScene = ns.AsyncLoading({
+                            width:        this.width,
+                            height:       this.height,
+                            assets:       MAIN_ASSET,
+                            loadingScene: ns.EffectLoadingScene,
+                            nextScene:    ns.MainScene,
+                        },function (postLoadingFunc) {
+                            var self = this;
+                            var socket = self.socket;
+                            socket.emit("getMapData", stairs);
+                            // mapデータ取得
+                            socket.on("gotMapData", function (data) {
+                                console.log("gotMapData");
+                                this.mapData = data;
+                                postLoadingFunc();
+                            }.bind(self));
+                        }.bind(ns.gameEvent)); // 仕方なくここで通信系の処理を書く
+                        this.app.replaceScene(loadingScene);
+
+                        break;
+                    default :
+                        break;
+                }
                 this.lastSelection = e.selectIndex;
             }.bind(this);
         },
